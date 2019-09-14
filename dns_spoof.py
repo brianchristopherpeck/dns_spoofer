6aa41ce07f70b72bf55f63724ea775ad93cdf28e
Brian Peck <brian.peck@thecontrolgroup.com>
2019-09-13 18:35:57 -0700

    If packet has DNS layer

--- dns_spoof.py
+++ dns_spoof.py
@@ -5,11 +5,12 @@ import scapy.all as scapy
 def process_packet(packet):
     # Prints packet payload
     scapy_packet = scapy.IP(packet.get_payload())
-    print(scapy_packet.show())
+    if scapy_packet.hasLayer(scapy.DNSRR):
+        print(scapy_packet.show())
     # Drops the packet... cuts connection of target client
-    packet.drop()
+    # packet.drop()
     # Intercept incoming packets and accept them instead of dropping them
-    # packet.accept
+    packet.accept()
 
 queue = netfilterqueue.NetfilterQueue()
 queue.bind(0, process_packet)
